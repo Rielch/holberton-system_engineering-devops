7e2bb7e49eb1bb862cf88f3c0bbc217cd7a28aac
Gabriel Prochnau <2288@holbertonschool.com>
2021-05-18 19:48:45 +0000

    Add part 1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""prints the titles of the top ten posts of a given subreddit"""
+import requests
+
+
+def top_ten(subreddit):
+    """prints the titles of the top ten posts of a given subreddit"""
+
+    r = requests.get("https://www.reddit.com/r/{}/hot.json?limit=10"
+                     .format(subreddit),
+                     allow_redirects=False,
+                     headers={"User-Agent": "Mozilla/5.0"})
+    if r.status_code != 200:
+        print(None)
+    for post in r.json().get("data").get("children"):
+        print(post.get("data").get("title"))
