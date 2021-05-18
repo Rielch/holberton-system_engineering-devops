a91db5131042b84a85d2e24fcd24db8a0e7be321
Gabriel Prochnau <2288@holbertonschool.com>
2021-05-18 18:57:06 +0000

    Fix par 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,4 +9,6 @@ def number_of_subscribers(subreddit):
     r = requests.get("https://www.reddit.com/r/{}/about.json".format(subreddit),
                      allow_redirects=False,
                      headers={"User-Agent": "Mozilla/5.0"})
+    if r.status_code != 200:
+        return 0
     return (r.json().get("data").get("subscribers"))
