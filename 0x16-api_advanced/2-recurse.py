b15347f3da719f5c6b2c86b2554ab5c4aff849b7
Gabriel Prochnau <2288@holbertonschool.com>
2021-05-19 00:07:41 +0000

    Add part 2

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Return a list of the titles of all hot posts in a subreddit"""
+import requests
+
+
+def recurse(subreddit, hot_list=[], count=0, after=""):
+    """Return  a list of the titles of all
+    hot posts in a subreddit recursively"""
+
+    r = requests.get("https://www.reddit.com/r/{}/hot.json\
+?count={:d}&after={}&limit=1"
+                     .format(subreddit, count, after), allow_redirects=False,
+                     headers={"User-Agent": "Mozilla/5.0"})
+    if r.status_code != 200:
+        return None
+    count += 1
+    after = r.json().get("data").get("after")
+    hot_list.append([r.json().get("data").get("children")[0]
+                     .get("data").get("title")])
+    hot_list = recurse(subreddit, hot_list, count, after)
+    return hot_list
