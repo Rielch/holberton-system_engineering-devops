f81c25111797801b19018d224a1e50135a066255
Gabriel Prochnau <2288@holbertonschool.com>
2021-05-19 00:19:58 +0000

    Fix part 2

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -3,10 +3,13 @@
 import requests
 
 
-def recurse(subreddit, hot_list=[], count=0, after=""):
+def recurse(subreddit, hot_list=[], count=0, after=None):
     """Return  a list of the titles of all
     hot posts in a subreddit recursively"""
 
+    if after != None:
+        if after == "STOP":
+            return hot_list
     r = requests.get("https://www.reddit.com/r/{}/hot.json\
 ?count={:d}&after={}&limit=1"
                      .format(subreddit, count, after), allow_redirects=False,
@@ -14,7 +17,9 @@ def recurse(subreddit, hot_list=[], count=0, after=""):
     if r.status_code != 200:
         return None
     count += 1
-    after = r.json().get("data").get("after")
+    after = r.json().get("data").get("after", "STOP")
+    if not after:
+        after = "STOP"
     hot_list.append([r.json().get("data").get("children")[0]
                      .get("data").get("title")])
     hot_list = recurse(subreddit, hot_list, count, after)
