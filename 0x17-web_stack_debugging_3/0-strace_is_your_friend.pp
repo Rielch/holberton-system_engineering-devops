5f56752379bd638c1470cfba2dcc4b9d2c41a2dd
Gabriel Prochnau <2288@holbertonschool.com>
2021-05-19 19:57:40 +0000

    Fix part 0

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 #Puppet command to fix te server in the project Web stack debugging 3
 exec { 'fix server':
-     command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php; sudo service apache2 restart'
-     path    => ['/bin', '/usr/bin', '/usr/sbin']
-     }
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php; sudo service apache2 restart',
+  path    => ['/bin', '/usr/bin', '/usr/sbin']
+}
